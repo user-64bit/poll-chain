fedafcdb7cb5894002e11f17a185f68b0b613d77
user-64bit <arth.prajapati@outlook.com>
2024-11-29 15:19:42 +0530

    initialize and create poll instructions

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,16 +1,113 @@
 use anchor_lang::prelude::*;
 
-declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
+declare_id!("DBCCbVg1LtpzghJiiUv9su2tYnj7mSUMGUeKyQjWtpRR");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
-pub mod basic {
+pub mod polly {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let poll_counter = &mut ctx.accounts.poll_counter;
+        poll_counter.count = 0;
+
+        let candidate_counter = &mut ctx.accounts.candidate_counter;
+        candidate_counter.count = 0;
+        Ok(())
+    }
+
+    pub fn create_poll(
+        ctx: Context<CreatePoll>,
+        title: String,
+        start_date: u64,
+        end_date: u64,
+    ) -> Result<()> {
+        if start_date >= end_date {}
+        let poll_counter = &mut ctx.accounts.poll_counter;
+        poll_counter.count += 1;
+
+        let poll = &mut ctx.accounts.poll;
+        poll.id = poll_counter.count;
+        poll.title = title;
+        poll.start_date = start_date;
+        poll.end_date = end_date;
+        poll.candidates = 0;
         Ok(())
     }
 }
 
+/* Below are the structs */
+
+// Couting number of polls and it helps to fetch all polls too.
+#[account]
+#[derive(InitSpace)]
+pub struct PollCounter {
+    pub count: u64,
+}
+
+// Couting number of candidates and it helps to fetch all candidates too.
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateCounter {
+    pub count: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + PollCounter::INIT_SPACE,
+        seeds = [b"poll_counter"],
+        bump
+    )]
+    pub poll_counter: Account<'info, PollCounter>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + CandidateCounter::INIT_SPACE,
+        seeds = [b"candidate_counter"],
+        bump
+    )]
+    pub candidate_counter: Account<'info, CandidateCounter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreatePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Poll::INIT_SPACE,
+        seeds = [(poll_counter.count + 1).to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        mut,
+        seeds = [b"poll_counter"],
+        bump
+    )]
+    pub poll_counter: Account<'info, PollCounter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub id: u64,
+    #[max_len(280)]
+    pub title: String,
+    pub start_date: u64,
+    pub end_date: u64,
+    pub candidates: u64,
+}
---
+++ anchor/target/types/polly.ts
@@ -0,0 +1,259 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/polly.json`.
+ */
+export type Polly = {
+  "address": "DBCCbVg1LtpzghJiiUv9su2tYnj7mSUMGUeKyQjWtpRR",
+  "metadata": {
+    "name": "polly",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "createPoll",
+      "discriminator": [
+        182,
+        171,
+        112,
+        238,
+        6,
+        219,
+        14,
+        110
+      ],
+      "accounts": [
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "poll",
+          "writable": true
+        },
+        {
+          "name": "pollCounter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  112,
+                  111,
+                  108,
+                  108,
+                  95,
+                  99,
+                  111,
+                  117,
+                  110,
+                  116,
+                  101,
+                  114
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "title",
+          "type": "string"
+        },
+        {
+          "name": "startDate",
+          "type": "u64"
+        },
+        {
+          "name": "endDate",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "pollCounter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  112,
+                  111,
+                  108,
+                  108,
+                  95,
+                  99,
+                  111,
+                  117,
+                  110,
+                  116,
+                  101,
+                  114
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "candidateCounter",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  99,
+                  97,
+                  110,
+                  100,
+                  105,
+                  100,
+                  97,
+                  116,
+                  101,
+                  95,
+                  99,
+                  111,
+                  117,
+                  110,
+                  116,
+                  101,
+                  114
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "candidateCounter",
+      "discriminator": [
+        84,
+        43,
+        21,
+        174,
+        69,
+        123,
+        237,
+        139
+      ]
+    },
+    {
+      "name": "poll",
+      "discriminator": [
+        110,
+        234,
+        167,
+        188,
+        231,
+        136,
+        153,
+        111
+      ]
+    },
+    {
+      "name": "pollCounter",
+      "discriminator": [
+        196,
+        1,
+        77,
+        116,
+        60,
+        205,
+        237,
+        189
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "candidateCounter",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "poll",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "id",
+            "type": "u64"
+          },
+          {
+            "name": "title",
+            "type": "string"
+          },
+          {
+            "name": "startDate",
+            "type": "u64"
+          },
+          {
+            "name": "endDate",
+            "type": "u64"
+          },
+          {
+            "name": "candidates",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "pollCounter",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
